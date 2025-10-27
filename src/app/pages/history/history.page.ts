49cb1c6b96c7416b37628a88024cd77a48e0bcb7
sammed <sammed.patil29@gmail.com>
2025-10-27 19:54:09 +0530

    added new logos

--- src/app/pages/history/history.page.ts
+++ src/app/pages/history/history.page.ts
@@ -16,6 +16,7 @@ import { Login } from 'src/app/services/login';
 export class HistoryPage implements OnInit {
 pastVisits: any = []
 titlecase: any;
+isLoading: boolean = false
   constructor(private navCtrl: NavController, private loginService: Login, private http: HttpClient) { }
 
   ngOnInit() {
@@ -26,11 +27,42 @@ titlecase: any;
     this.navCtrl.back()
   }
 
+  gotoAdd(){
+    this.navCtrl.navigateRoot('/layout/add-visit')
+  }
+
   async getVisits(){
+    this.isLoading = true
     const token = await localStorage.getItem('userToken')
     this.loginService.getVisits({token}).subscribe((res:any) => {
       this.pastVisits = res.user_visits.reverse()
+      this.isLoading = false
+    }, error => {
+      this.isLoading = false
     })
   }
+
+  openMap(lat: number, lng: number) {
+  // Construct the Google Maps URL
+  const url = `https://www.google.com/maps/search/?api=1&query=${lat},${lng}`;
+  console.log(url)
+
+  // Open in new tab (browser) or in system app on mobile
+  window.open(url, '_blank');
+}
+
+images: any = []
+showPreview: boolean = false
+openImage(item: any) {
+  this.showPreview = true;
+
+  // Clone locationImage to avoid modifying original
+  this.images = [...item.locationImage];
+
+  // Add selfie only once
+  this.images.push(item.selfie);
+}
+
+
 
 }
